ad7dd5ab0f24cce33ccc1a1e5c892ef367565135
Tianruo <1292827871@qq.com>
2017-05-05 10:40:02 +0800

    funtion

--- recorder.js
+++ recorder.js
@@ -221,6 +221,40 @@ function timecode(ms) {
         tc.push((hms.s < 10  ? "0" + hms.s : hms.s));
         return tc.join(':');
 }
+function record(){
+        Recorder.record({
+          start: function(){
+            //alert("recording starts now. press stop when youre done. and then play or upload if you want.");
+          },
+          progress: function(milliseconds){
+            document.getElementById("time").innerHTML = timecode(milliseconds);
+          }
+        });
+      }
+      
+      function play(){
+        Recorder.stop();
+        Recorder.play({
+          progress: function(milliseconds){
+            document.getElementById("time").innerHTML = timecode(milliseconds);
+          }
+        });
+      }
+      
+      function stop(){
+        Recorder.stop();
+      }
+      
+      function upload(){
+        Recorder.upload({
+          url:        "https://example.com/upload",
+          //url:        "https://github.com/",
+          audioParam: "your_file",
+          success: function(){
+            alert("your file was uploaded!");
+          }
+        });
+      }
 
 if(swfobject==undefined){
   /*	SWFObject v2.2 <http://code.google.com/p/swfobject/> is released under the MIT License <http://www.opensource.org/licenses/mit-license.php */
